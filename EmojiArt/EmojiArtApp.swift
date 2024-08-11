79aad85b0b59d22214408e502fd7994501fd623e
donggeon Na <ndmb2012@gmail.com>
2024-08-11 12:13:42 +0900

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by 나동건 on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
